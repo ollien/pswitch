e52431fbbad440150b43a1ee2261c31f0c9944d3
Nick Krichevsky <nick@ollien.com>
2017-06-25 22:04:09 -0400

    Change name in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="pulse-switch",
+    name="pswitch",
     version="1.0",
     author="ollien",
     author_email="nick@ollien.com",
-    url="https://github.com/ollien/pulseswitch",
-    packages=["pulseswitch"],
+    url="https://github.com/ollien/pswitch",
+    packages=["pswitch"],
     entry_points={
         "console_scripts": [
-            "pswitch=pulseswitch.__main__:main"
+            "pswitch=pswitch.__main__:main"
         ]
     }
 )
