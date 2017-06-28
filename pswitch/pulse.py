adb3d9ef5c4a8fec401de0aff44a5389d7f9b65e
Nick Krichevsky <nick@ollien.com>
2017-06-27 21:43:52 -0400

    Add docstrings to pulse.py

--- pswitch/pulse.py
+++ pswitch/pulse.py
@@ -12,17 +12,47 @@ DEVICE_DESCRIPTION_REGEX = re.compile(r"\t\tdevice.description\s=\s\"(.*)\"")
 
 
 def get_sources():
+    """Get all sources from Pulse Audio.
+
+    Returns:
+        Returns a list of dicts. Each dict contains the sink's index in
+        Pulse Audio (with key 'pulse_index'), the source's name
+        (with key 'device_name') and whether or not it's actively the
+        default source (with key 'active').
+    """
+
     pacmd_output = subprocess.check_output("pacmd list-sources", shell=True)
     return parse_pacmd_list_output(pacmd_output)
 
 
 def get_sinks():
+    """Get all sinks from Pulse Audio.
+
+    Returns:
+        Returns a list of dicts. Each dict contains the sink's index in
+        Pulse Audio (with key 'pulse_index'), the sink's name (with key
+        'device_name') and whether or not it's actively the default sink
+        (with key 'active').
+    """
+
     pacmd_output = subprocess.check_output("pacmd list-sinks", shell=True)
     return parse_pacmd_list_output(pacmd_output)
 
 
-# Will parse output from pacmd list-sinks and pacmd list-sources
 def parse_pacmd_list_output(pacmd_output):
+    """Parse the output from pacmd list-sinks and pacmd list-sources.
+
+    Args:
+        pacmd_output (str): the output from either pacmd list-sinks
+            or pacmd list-sources.
+
+    Returns:
+        Returns a list of dicts. Each dict contains the sink's/source's
+        index in Pulse Audio (with key 'pulse_index'), the
+        sink's/source's name (with key 'device_name') and whether or not
+        it's actively the default source/sink (with key 'active').
+    """
+
     raw_devices = re.split(DEVICE_INDEX_REGEX,
                            pacmd_output.decode("utf-8"))[1:]
     devices = []
@@ -48,19 +78,41 @@ def parse_pacmd_list_output(pacmd_output):
 
 
 def get_sink_input_indexes():
+    """Gets the indexes of all sinks.
+
+    Returns:
+        List of integers representing the indexes of the sink inputs.
+    """
+
     pacmd_output = subprocess.check_output("pacmd list-sink-inputs",
                                            shell=True)
     return get_indexes_from_pacmd_output(pacmd_output)
 
 
 def get_source_output_indexes():
+    """Gets the indexes of all sources.
+
+    Returns:
+        List of integers representing the indexes of the source outputs.
+    """
+
     pacmd_output = subprocess.check_output("pacmd list-source-outputs",
                                            shell=True)
     return get_indexes_from_pacmd_output(pacmd_output)
 
 
-# Will parse output from pacmd list-source-outputs and pacmd list-sink-outputs
 def get_indexes_from_pacmd_output(pacmd_output):
+    """Parses output from pacmd list-source-outputs and pacmd list-sink-inputs.
+
+    Args:
+        pacmd_output (str): Otuput from pacmd list-sink-inputs or
+            pacmd list-source-outputs.
+
+    Returns:
+        List of integers representing the indexes of the sink inputs or
+        source outputs
+    """
+
     index_matches = re.finditer(DEVICE_INDEX_REGEX,
                                 pacmd_output.decode("utf-8"))
     return [int(index.groups()[0]) for index in index_matches]
@@ -96,6 +148,14 @@ def set_default_source(source_index):
 
 
 def switch_to_sink(sink_index):
+    """Iterates through all sink inputs and moves them to the sink
+    specified by sink_index.
+
+    Args:
+        sink_index (int): The index of the sink that inputs should
+            be moved to.
+    """
+
     set_default_sink(sink_index)
     inputs = get_sink_input_indexes()
     for input_index in inputs:
@@ -103,6 +163,14 @@ def switch_to_sink(sink_index):
 
 
 def switch_to_source(source_index):
+    """Iterates through all source outputs and moves them to the source
+    specified by source_index.
+
+    Args:
+        source_index (int): The index of the source that outputs should
+            be moved to.
+    """
+
     set_default_source(source_index)
     outputs = get_source_output_indexes()
     for output_index in outputs:
